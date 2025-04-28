60645737055d0c63b2375eb4757871d9ff0596cf
batistadaniel <batistanegobiz@gmail.com>
2025-04-28 11:57:05 -0300

    prova git

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Alo Git!");
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.println("\t\tJStatistics");
+    System.out.print("-- Controle de Dados Empresarial -- ");
+    System.out.print("2025");
+
+    // ArrayList lista = new ArrayList();
+
+    // String texto = "Centralizado";
+    // // int larguraTotal = 20; // Largura total da saída
+    // String textoCentralizado = String.format("%" + 30 + "s", texto);
+    // System.out.println(textoCentralizado);
+  }
+
 }
