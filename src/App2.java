e0f17a3d7be007d1fd398636253ad831f047b950
ead <ead@academico.local>
2025-05-08 08:28:54 -0300

    Segundo codigo excluindo um numero

--- src/App2.java
+++ src/App2.java
@@ -87,7 +87,7 @@ public class App2 {
                     // case 6:
                     //     cont6++;  
                     //     break;
-                    // EXCLUINOD O 6 DO ENQCREMENTO TBM
+                    // EXCLUINOD O 6 DO ENCREMENTO TBM
                     default:
                         break;
                 }
