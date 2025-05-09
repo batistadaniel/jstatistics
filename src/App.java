085259e2b6614938deb89912c551b4415a65429b
batistadaniel <batistanegobiz@gmail.com>
2025-05-09 11:13:58 -0300

    Resposta Exercicio 2

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
 
 public class App {
@@ -17,6 +19,12 @@ public class App {
         double valorLargura = 0;
         double valorCompromento = 0;
         double valorArea = 0;
+        double tamanhoPequeno = 50;
+        String[] listaServicos = {"Corte de grama", "Plantio de flores", "Poda de arvores", "Limpeza de jardim"};
+        // String[] listaServicosSolicitados = {};
+        ArrayList<String> listaServicosSolicitados = new ArrayList<>();
+        int escolhaServico;
+        String tituloDesconto = "DESCONTOS!!!";
 
         // Boas-vindas
         JOptionPane.showMessageDialog(null, msgBemVindo, tituloBemVindo, 1);
@@ -28,9 +36,20 @@ public class App {
         // Exibindo as informacoes
         JOptionPane.showMessageDialog(null, " #####  Suas informacoes #####\n\n\n" + "Nome: " + nomeCliente + "\n" + "Endereco: " + enderecoCliente, tituloCadastro, 3);
         
-        // Calculando a area do jardim
+        // Calculando a area do jardim.
         valorLargura = Double.parseDouble(JOptionPane.showInputDialog(null, entradaLargura, tituloCalculoArea, 3));
         valorCompromento = Double.parseDouble(JOptionPane.showInputDialog(null, entradaComprimento, tituloCalculoArea, 3));
+
+        do { // Validando campos de area
+            if (valorLargura < 0) {
+                valorLargura = Double.parseDouble(JOptionPane.showInputDialog(null, "Valor invalido!!!\n\n" + entradaLargura, tituloCalculoArea, 3));
+            } else if (valorCompromento < 0) {
+                valorCompromento = Double.parseDouble(JOptionPane.showInputDialog(null, "Valor invalido!!!\n\n" + entradaComprimento, tituloCalculoArea, 3));
+            } else {
+                break;
+            }
+        } while (valorLargura > 0 && valorCompromento > 0);
+        
         valorArea = (valorLargura * valorCompromento);
 
         // Exibindo o valor da area do jardim
@@ -38,7 +57,72 @@ public class App {
 
         // Parte 2
 
-        
+        if (valorArea <= tamanhoPequeno) {
+            JOptionPane.showMessageDialog(null, "O tamanho do seu jardim foi considerado pequeno!", tituloCalculoArea, 1);
+        } else {
+            JOptionPane.showMessageDialog(null, "O tamanho do seu jardim foi considerado grande!", tituloCalculoArea, 1);
+        }
 
+        // Menu de servicos
+        do {
+            escolhaServico = Integer.parseInt(JOptionPane.showInputDialog(null," ---------- Escolha um servico ----------\n\n" + 
+                    "\nServico 1: " + listaServicos[0]
+                    + "\nServico 2: " + listaServicos[1]
+                    + "\nServico 3: " + listaServicos[2]
+                    + "\nServico 4: " + listaServicos[3]
+                    + "\nSAIR - 5: "
+                ));
+
+            if (escolhaServico !=5) {
+                switch (escolhaServico) {
+                    case 1:
+                        listaServicosSolicitados.add(listaServicos[0]);
+                        break;
+                    case 2:
+                        listaServicosSolicitados.add(listaServicos[1]);
+                        break;
+                    case 3:
+                        listaServicosSolicitados.add(listaServicos[2]);
+                        break;
+                    case 4:
+                        listaServicosSolicitados.add(listaServicos[3]);
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                break;
+            }
+        } while (true);
+
+        // Desconto fidelidade
+        JOptionPane.showMessageDialog(null, " ----- Desconto por fidelidade -----\n\n" 
+                        + "Apenas 1 Servico: 0%\n" 
+                        + "2 Servicos: 5% de desconto\n"
+                        + "3 Servicos: 10% de desconto\n" 
+                        + "4 ou mais Servicos: 15% de desconto\n"
+                        + "Total de Servicos: " + listaServicosSolicitados.size() , tituloDesconto, 1);
+
+        switch (listaServicosSolicitados.size()) {
+            case 1:
+                JOptionPane.showMessageDialog(null, "Voce ganhou 0% de desconto. Que pena!" , tituloDesconto, 1);
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "Voce ganhou 5% de desconto. Parabens!" , tituloDesconto, 1);
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null, "Voce ganhou 10% de desconto. Parabens!" , tituloDesconto, 1);
+                break;
+            case 4:
+                JOptionPane.showMessageDialog(null, "Voce ganhou 15% de desconto. Parabens!" , tituloDesconto, 1);
+                break;
+            default:
+                break;
+        }
+
+        if (listaServicosSolicitados.size() > 4) {
+            JOptionPane.showMessageDialog(null, "Voce ganhou 15% de desconto. Parabens!" , tituloDesconto, 1);
+            
+        }
     }
 }
