4ab1c09f268ecc14a81ea27122429255e3089f5f
batistadaniel <batistanegobiz@gmail.com>
2025-05-09 08:58:40 -0300

    Resposta Exercicio 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Finalizei as aulas de logica.");
+
+        // Declarando as variaveis
+        String msgBemVindo = "Alô, Jardim!";
+        String tituloBemVindo = "Bem Vindo";
+        String tituloCadastro = "Cadastro";
+        String entradaNome = "Digite o seu nome: ";
+        String entradaEmdereco = "Digite o seu endereco: ";
+
+        // Boas-vindas
+        JOptionPane.showMessageDialog(null, msgBemVindo, tituloBemVindo, 1);
+        
+        // Cadastro de cliente
+        String nomeCliente = JOptionPane.showInputDialog(null, entradaNome, tituloCadastro, 1);
+        String enderecoCliente = JOptionPane.showInputDialog(null, entradaEmdereco, tituloCadastro, 1);
+
+        // Exibindo as informacoes
+        JOptionPane.showMessageDialog(null, " #####  Suas informacoes #####\n\n\n" + "Nome: " + nomeCliente + "\n" + "Endereco: " + enderecoCliente, tituloCadastro, 3);
+        
+
     }
 }
