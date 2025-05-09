bb6fef5c8e5a578011e2113e364a57dfebcd6541
batistadaniel <batistanegobiz@gmail.com>
2025-05-09 09:21:52 -0300

    Resposta Exercicio 1

--- src/App.java
+++ src/App.java
@@ -9,6 +9,12 @@ public class App {
         String tituloCadastro = "Cadastro";
         String entradaNome = "Digite o seu nome: ";
         String entradaEmdereco = "Digite o seu endereco: ";
+        String tituloCalculoArea = "Calculo de Area";
+        String entradaLargura = "Digite a largura: ";
+        String entradaComprimento = "Digite o comprimento: ";
+        double valorLargura = 0;
+        double valorCompromento = 0;
+        double valorArea = 0;
 
         // Boas-vindas
         JOptionPane.showMessageDialog(null, msgBemVindo, tituloBemVindo, 1);
@@ -20,6 +26,12 @@ public class App {
         // Exibindo as informacoes
         JOptionPane.showMessageDialog(null, " #####  Suas informacoes #####\n\n\n" + "Nome: " + nomeCliente + "\n" + "Endereco: " + enderecoCliente, tituloCadastro, 3);
         
+        // Calculando a area do jardim
+        valorLargura = Double.parseDouble(JOptionPane.showInputDialog(null, entradaLargura, tituloCalculoArea, 3));
+        valorCompromento = Double.parseDouble(JOptionPane.showInputDialog(null, entradaComprimento, tituloCalculoArea, 3));
+        valorArea = (valorLargura * valorCompromento);
 
+        // Exibindo o valor da area do jardim
+        JOptionPane.showMessageDialog(null, "O valor da area do jardim e de " + String.format("%.2f", valorArea) + " m² !", tituloCalculoArea, 1);
     }
 }
